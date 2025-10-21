664a3747f9235a489a3aa01f801ad329673e0e88
jacobrak <155147415+jacobrak@users.noreply.github.com>
2025-10-21 23:14:46 +0200

    vscode sum

--- sum.cpp
+++ sum.cpp
@@ -6,9 +6,16 @@ using namespace std;
 int main(){
     float sum;
     string output;
+    try{
     while (getline(cin, output)){
         sum += stof(output);
-    }    
+        }
     cout << sum << endl;
     return 0;
+    }  
+    catch (invalid_argument){
+        cerr << "bad input";
+        exit(1);
+    }
+    
 }
